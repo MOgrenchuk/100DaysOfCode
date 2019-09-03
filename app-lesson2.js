1318b36af74362f03c2335fbfbd40659f9887bb9
Maxim Ogrenchuk <m@ogrenchuk.pro>
2019-09-03 23:47:01 +0900

    firstDayResult
    Start

---
+++ app-lesson2.js
@@ -0,0 +1,8 @@
+let first = 3;
+let second = 5;
+let third = 11;
+
+console.log("first =", first, ", second =", second);
+// console.log(second);
+second = third;
+console.log(first + second, console);
